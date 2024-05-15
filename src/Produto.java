cd18fb1f3b930178a074fa4798f164aa9f32005e
theomaceres <theocamacho@hotmail.com>
2024-05-15 16:00:53 -0300

    Criada classe Produto e classe Servico

--- src/Produto.java
+++ src/Produto.java
@@ -1,2 +1,21 @@
-public class Produto {
+public class Produto extends Item{
+    private int estoque;
+
+    public Produto(float preco, float custo, String nome, int estoque){
+        super(preco, custo, nome);
+        this.estoque = estoque;
+    }
+
+    public int getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(int estoque) {
+        this.estoque = estoque;
+    }
+
+    public String toString(){
+        String out = " ";
+        return out;
+    }
 }
