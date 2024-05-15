d610e0ca4a0ad631a05ae4fee5bf4d1a07f51a2f
theomaceres <theocamacho@hotmail.com>
2024-05-15 18:20:52 -0300

    Cliente feito

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Cliente {
     private String cpf;
@@ -35,8 +36,40 @@ public class Cliente {
     }
 
     public boolean fazerPedido(){
-        int i=0;
-        System.out.println("Selecione o produto:");
+        int i=0, acao_p=0, acao_s = 0, selecionado=10;
+        Scanner scanner = new Scanner(System.in);
+        while (selecionado != 0) {
+            System.out.println("Selecione:\n (1) Produtos\n (2) Serviços\n (0) Voltar");
+            selecionado = scanner.nextInt();
+            if (selecionado == 1) {
+                System.out.println("Selecione o produto:");
+                for (i = 0; i < Mecanica.getProdutos().size(); i++) {
+                    System.out.println("(" + (i + 1) + ") " + Mecanica.getProdutos().get(i).getNome());
+                }
+                System.out.println("(0) Voltar");
+                acao_p = scanner.nextInt();
+                if (acao_p != 0) {
+                    System.out.println("Digite a quantidade: ");
+                    int quantidade = scanner.nextInt();
+                    if (quantidade > Mecanica.getProdutos().get(acao_p - 1).getEstoque()) {
+                        System.out.println("Sem estoque");
+                        acao_p = 0;
+                    }
+                    Pedido newPedido = new Pedido(Mecanica.getProdutos().get(acao_p-1).getNome(), quantidade);
+                }
+            } else if (selecionado == 2) {
+                System.out.println("Selecione o serviço: ");
+                for (i = 0; i < Mecanica.getServicos().size(); i++) {
+                    System.out.println("(" + (i + 1) + ") " + Mecanica.getProdutos().get(i).getNome());
+                }
+                System.out.println("(0) Voltar");
+                acao_s = scanner.nextInt();
+                Pedido newPedido = new Pedido(Mecanica.getServicos().get(acao_s-1).getNome(), 0);
+            }
+        }
+
+        //acao_p-1 indice do produto, acao_s indice do serviço
+
         return true;
     }
 }
